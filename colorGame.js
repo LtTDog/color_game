1f65ad3dc3bf26678549924afb96fcfd2d6c7136
Terrence Drumm <terrence.drumm@gmail.com>
2017-06-28 15:34:11 -0400

    reset

--- colorGame.js
+++ colorGame.js
@@ -4,6 +4,24 @@ let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
+let resetButton = document.querySelector("#reset");
+
+let h1 = document.querySelector("h1");
+
+resetButton.addEventListener("click", function(){
+    //generate all new colors
+    colors = generateRandomColors(6);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colorDisplay to match picked color
+    colorDisplay.textContent = pickedColor;
+    //change colors of squares
+    h1.style.backgroundColor = "#232323";
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+})
+
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,6 +38,8 @@ for (let i = 0; i < squares.length; i++) {
         {
             messageDisplay.textContent = "Correct";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play Again?"
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
